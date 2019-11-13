4f528e6537df78874dbd98dbad43cc39827bf523
Amanda Hinchman-Dominguez <ahinchman1@gmail.com>
2019-11-13 13:12:14 -0600

    TypeElement quote and template scope
    removed comments for incorrect scope

--- compiler-plugin/src/main/kotlin/arrow/meta/quotes/BinaryExpression.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/quotes/BinaryExpression.kt
@@ -2,11 +2,8 @@ package arrow.meta.quotes
 
 import arrow.meta.Meta
 import arrow.meta.phases.ExtensionPhase
-import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtBinaryExpression
-import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
 
 /**
@@ -16,8 +13,11 @@ fun Meta.binaryExpression(
   match: KtBinaryExpression.() -> Boolean,
   map: BinaryExpressionScope.(KtBinaryExpression) -> Transform<KtBinaryExpression>
 ): ExtensionPhase =
-  quote(match, map) { BinaryExpressionScope(it) } // How can I drill down into the right scope here?
+  quote(match, map) { BinaryExpressionScope(it) }
 
+/**
+ * A template destructuring [Scope] for a [KtBinaryExpression]
+ */
 class BinaryExpressionScope(
   override val value: KtBinaryExpression,
   val left: Scope<KtExpression>? = Scope(value.left),
